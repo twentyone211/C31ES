b6602974e6c55718a77f8260a67d02305ceb5f1e
Frank Schuetze <Frank.Schuetze@htwb.onmicrosoft.com>
2020-06-04 14:48:39 +0200

    LCD_init

--- Src/main.c
+++ Src/main.c
@@ -208,6 +208,59 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
+
+
+#define LCD_COLOR_BLUE          ((uint32_t)0xFF0000FF)
+#define LCD_COLOR_GREEN         ((uint32_t)0xFF00FF00)
+#define LCD_COLOR_RED           ((uint32_t)0xFFFF0000)
+#define LCD_COLOR_CYAN          ((uint32_t)0xFF00FFFF)
+#define LCD_COLOR_MAGENTA       ((uint32_t)0xFFFF00FF)
+#define LCD_COLOR_YELLOW        ((uint32_t)0xFFFFFF00)
+#define LCD_COLOR_LIGHTBLUE     ((uint32_t)0xFF8080FF)
+#define LCD_COLOR_LIGHTGREEN    ((uint32_t)0xFF80FF80)
+#define LCD_COLOR_LIGHTRED      ((uint32_t)0xFFFF8080)
+#define LCD_COLOR_LIGHTCYAN     ((uint32_t)0xFF80FFFF)
+#define LCD_COLOR_LIGHTMAGENTA  ((uint32_t)0xFFFF80FF)
+#define LCD_COLOR_LIGHTYELLOW   ((uint32_t)0xFFFFFF80)
+#define LCD_COLOR_DARKBLUE      ((uint32_t)0xFF000080)
+#define LCD_COLOR_DARKGREEN     ((uint32_t)0xFF008000)
+#define LCD_COLOR_DARKRED       ((uint32_t)0xFF800000)
+#define LCD_COLOR_DARKCYAN      ((uint32_t)0xFF008080)
+#define LCD_COLOR_DARKMAGENTA   ((uint32_t)0xFF800080)
+#define LCD_COLOR_DARKYELLOW    ((uint32_t)0xFF808000)
+#define LCD_COLOR_WHITE         ((uint32_t)0xFFFFFFFF)
+#define LCD_COLOR_LIGHTGRAY     ((uint32_t)0xFFD3D3D3)
+#define LCD_COLOR_GRAY          ((uint32_t)0xFF808080)
+#define LCD_COLOR_DARKGRAY      ((uint32_t)0xFF404040)
+#define LCD_COLOR_BLACK         ((uint32_t)0xFF000000)
+#define LCD_COLOR_BROWN         ((uint32_t)0xFFA52A2A)
+#define LCD_COLOR_ORANGE        ((uint32_t)0xFFFFA500)
+#define LCD_COLOR_TRANSPARENT   ((uint32_t)0xFF000000)
+
+#define LCD_FB_START_ADDRESS       ((uint32_t)0xC0000000)
+#define LTDC_ACTIVE_LAYER	     ((uint32_t)1) /* Layer 1 */
+
+  typedef enum
+  {
+    CENTER_MODE             = 0x01,    /* Center mode */
+    RIGHT_MODE              = 0x02,    /* Right mode  */
+    LEFT_MODE               = 0x03     /* Left mode   */
+  }Text_AlignModeTypdef;
+
+  char str1[] = "Frank";
+  char str2[] = "Lukas";
+  char str3[] = "Philipp";
+
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
+  BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+  BSP_LCD_DisplayOn();
+  BSP_LCD_Clear(LCD_COLOR_ORANGE);
+  BSP_LCD_DisplayStringAt(0, 10, str1, LEFT_MODE);
+  BSP_LCD_DisplayStringAt(20, 110, str2, CENTER_MODE);
+  BSP_LCD_DisplayStringAt(50, 220, str3, RIGHT_MODE);
+
   while (1)
   {
     /* USER CODE END WHILE */
